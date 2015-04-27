1a692fee7fa696ec44328827e652c2cc06cfd6f9
Saynur Demirdelen <saynurdemirdelen@Locals-MacBook-Air.local>
2015-04-28 01:14:50 +0300

    initial

--- talkwithme/ViewController.swift
+++ talkwithme/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  talkwithme
 //
-//  Created by Saynur Demirdelen on 28/04/15.
+//  Created by Baris Demirdelen on 28/04/15.
 //  Copyright (c) 2015 Baris Demirdelen. All rights reserved.
 //
 
@@ -10,10 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var webView: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSURL (string: "http://barisdemirdelen.com/chat/");
+        let requestObj = NSURLRequest(URL: url!);
+        webView.loadRequest(requestObj);
     }
+    /*
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated);
+        webView.frame = self.view.frame;
+    }*/
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
